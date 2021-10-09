2a87881268668777d1371263edc3885a1b92de01
WoodyHazel <66266993+WoodyHazel@users.noreply.github.com>
2021-10-09 09:11:35 -0400

    10/9 9am

--- main.js
+++ main.js
@@ -75,8 +75,83 @@ showcaseRightArrow.addEventListener("click", (e) => {
 });
 
 // Offerings
+const card1Details = document.querySelector(".card1-details");
+const card1DetailsBtn = document.querySelector(".card1-details-btn");
+const card1Pricing = document.querySelector(".card1-pricing");
+const card1PricingBtn = document.querySelector(".card1-pricing-btn");
 
-// GALLERY
+const card2Details = document.querySelector(".card2-details");
+const card2DetailsBtn = document.querySelector(".card2-details-btn");
+const card2Pricing = document.querySelector(".card2-pricing");
+const card2PricingBtn = document.querySelector(".card2-pricing-btn");
+
+const card3Details = document.querySelector(".card3-details");
+const card3DetailsBtn = document.querySelector(".card3-details-btn");
+const card3Pricing = document.querySelector(".card3-pricing");
+const card3PricingBtn = document.querySelector(".card3-pricing-btn");
+
+card1DetailsBtn.addEventListener("click", () => {
+  if (card1Details.classList.contains("hidden-down")) {
+    if (!card1Pricing.classList.contains("hidden-down")) {
+      card1Pricing.classList.add("hidden-down");
+    }
+    card1Details.classList.remove("hidden-down");
+  } else {
+    card1Details.classList.add("hidden-down");
+  }
+});
+card1PricingBtn.addEventListener("click", () => {
+  if (card1Pricing.classList.contains("hidden-down")) {
+    if (!card1Details.classList.contains("hidden-down")) {
+      card1Details.classList.add("hidden-down");
+    }
+    card1Pricing.classList.remove("hidden-down");
+  } else {
+    card1Pricing.classList.add("hidden-down");
+  }
+});
+
+card2DetailsBtn.addEventListener("click", () => {
+  if (card2Details.classList.contains("hidden-down")) {
+    if (!card2Pricing.classList.contains("hidden-down")) {
+      card2Pricing.classList.add("hidden-down");
+    }
+    card2Details.classList.remove("hidden-down");
+  } else {
+    card2Details.classList.add("hidden-down");
+  }
+});
+card2PricingBtn.addEventListener("click", () => {
+  if (card2Pricing.classList.contains("hidden-down")) {
+    if (!card2Details.classList.contains("hidden-down")) {
+      card2Details.classList.add("hidden-down");
+    }
+    card2Pricing.classList.remove("hidden-down");
+  } else {
+    card2Pricing.classList.add("hidden-down");
+  }
+});
+card3DetailsBtn.addEventListener("click", () => {
+  if (card3Details.classList.contains("hidden-down")) {
+    if (!card3Pricing.classList.contains("hidden-down")) {
+      card3Pricing.classList.add("hidden-down");
+    }
+    card3Details.classList.remove("hidden-down");
+  } else {
+    card3Details.classList.add("hidden-down");
+  }
+});
+card3PricingBtn.addEventListener("click", () => {
+  if (card3Pricing.classList.contains("hidden-down")) {
+    if (!card3Details.classList.contains("hidden-down")) {
+      card3Details.classList.add("hidden-down");
+    }
+    card3Pricing.classList.remove("hidden-down");
+  } else {
+    card3Pricing.classList.add("hidden-down");
+  }
+});
+// Gallery
 const extendGalleryBtn = document.querySelector(".extend-gallery");
 const collapseGalleryBtn = document.querySelector(".collapse-gallery");
 const extendedGallery = document.querySelector(".extended-gallery-container");
